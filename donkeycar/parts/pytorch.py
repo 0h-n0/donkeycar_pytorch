72bcaecb6706b8ad3159848146a4ac43fbcd2cd3
Will Roscoe <wroscoe@gmail.com>
2018-11-28 14:32:57 -0800

    Update donkey2.py

---
+++ donkeycar/parts/pytorch.py
@@ -0,0 +1,55 @@
+""""
+
+pytorch.py
+
+functions to run and train autopilots using pytorch
+
+"""
+
+import donkey_torch
+
+class TorchPilot:
+
+    def load(self, model_path):
+        self.model = load_model(model_path)
+
+    def shutdown(self):
+        pass
+
+    def train(self, train_gen, val_gen,
+              saved_model_path, epochs=100, steps=100, train_split=0.8,
+              verbose=1, min_delta=.0005, patience=5, use_early_stop=True):
+        """
+        train_gen: generator that yields an array of images an array of
+
+        """
+
+        # checkpoint to save model after each epoch
+        save_best = ModelCheckpoint(saved_model_path,
+                                    monitor='val_loss',
+                                    verbose=verbose,
+                                    save_best_only=True,
+                                    mode='min')
+
+        # stop training if the validation error stops improving.
+        early_stop = EarlyStopping(monitor='val_loss',
+                                   min_delta=min_delta,
+                                   patience=patience,
+                                   verbose=verbose,
+                                   mode='auto')
+
+        callbacks_list = [save_best]
+
+        if use_early_stop:
+            callbacks_list.append(early_stop)
+
+        hist = self.model.fit_generator(
+            train_gen,
+            steps_per_epoch=steps,
+            epochs=epochs,
+            verbose=1,
+            validation_data=val_gen,
+            callbacks=callbacks_list,
+            validation_steps=steps * (1.0 - train_split) / train_split)
+        return hist
+
